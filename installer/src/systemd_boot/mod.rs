61401fa540acd1caebd895cf5b3f2df1960e65e0
Cole Helbling <cole.helbling@determinate.systems>
2021-08-18 09:46:35 -0700

    installer: remove "guidance" comments

--- installer/src/systemd_boot/mod.rs
+++ installer/src/systemd_boot/mod.rs
@@ -1,37 +1,3 @@
-// I'm imagining the installer will create e.g. systemd-boot's loader.conf
-// and grub's grub.cfg (amongst other required files) and add it to the
-// generated bootloader/ folder, then add it to the store, then update the
-// bootloader profile to point to that store path
-//
-// bootloader profile should only consist of the generated entries?
-//
-// installer will take generated entries from /nix/var/nix/profiles/bootloader,
-// atomically switch all entries (using .tmp and `mv` for systemd), then add
-// the bootloader-specific config to the boot partition directly? hardware
-// and related config will always be taken from the current system...
-//
-// a snapshot of the bootloader-specific files that should go into /boot
-
-// collect a list of entries that we generate and remove old ones from ESP/loader/entries:
-//     gens = get_generations()
-//     for profile in get_profiles():
-//         gens += get_generations(profile)
-//     remove_old_entries(gens)
-
-/*
-1a. only (bare) arg is the path to the default / just-built toplevel
-1b. maybe accept flags for stuff like timeout, etc, that goes into the config
-2. read machine_id and append it to all entries? (thus removing machine_id handling from the generator... but that's not the best)
-3a. NIXOS_INSTALL_GRUB and NIXOS_INSTALL_BOOTLOADER
-3b. if N_I_B and loader/loader.conf exists in ESP (destination), remove it
-3c. if canTouchEfiVars, bootctl install, else bootctl install --no-variables
-4. else, update to latest version of sd-boot (compare systemd and installed sd-boot versions)
-5. get a list of entries to generate, also check profiles, and remove old entries
-6. write loader conf if one of the generations' store dir (realpath) matches the toplevel we were passed
-7. special-case memtest? bleh
-8. syncfs to make sure a crash/outage doesn't make the system unbootable
-*/
-
 use std::ffi::{CStr, OsString};
 use std::fmt::Write as _;
 use std::fs::{self, File};
