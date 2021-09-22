a45900762ae0958a205928e8ec17c55886853b53
Cole Helbling <cole.helbling@determinate.systems>
2021-09-22 11:36:30 -0700

    generator: switch to structopt

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,45 +1,28 @@
-// this just creates generation boot configs
-// accepts a list of system profiles / generations
-
-// TODO: better error handling
-//  -> just replace unwraps with expects for now
-
-// to create the bootloader profile:
-// (do this in the installer package?)
-// 1. cd (mktemp -d)
-// 2. run this to get boot/entries/...
-// 3. nix-store --add ./somepath
-// 4a. make sure bootloader profile doesn't exist (or is ours)?
-// 4b. then nix-env -p /nix/var/nix/profiles/bootloader --set ...
-
-// TODO: think about user flows, how the tool should behave
-// use cases:
-//   * generating bootloader entries to install (if supported / necessitated by the bootloader)
-
-// boot.loader.manual.enable = true; <- stubs out the `installBootloader` script to say "OK, update your bootloader now!\n  {path to bootspec.json}"
-
 use std::path::PathBuf;
 
 use generator::bootable::{self, Bootable, EfiProgram};
 use generator::{systemd_boot, Generation, Result};
+use structopt::StructOpt;
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, StructOpt)]
 struct Args {
     // TODO: --out-dir?
     /// The systemd-boot EFI stub used to create a unified EFI file
+    #[structopt(long, requires_all = &["objcopy", "unified-efi"])]
     systemd_efi_stub: Option<PathBuf>,
     /// The `objcopy` binary
-    ///
-    /// Required if `--unified-efi` is provided
+    #[structopt(long, requires_all = &["systemd-efi-stub", "unified-efi"])]
     objcopy: Option<PathBuf>,
     /// Whether or not to combine the initrd and kernel into a unified EFI file
+    #[structopt(long, requires_all = &["systemd-efi-stub", "objcopy"])]
     unified_efi: bool,
     /// A list of generations in the form of `/nix/var/nix/profiles/system-*-link`
+    #[structopt(required = true)]
     generations: Vec<String>,
 }
 
 fn main() -> Result<()> {
-    let args = self::parse_args()?;
+    let args = Args::from_args();
 
     let generations = args
         .generations
@@ -70,42 +53,4 @@ fn main() -> Result<()> {
     // grub::generate(bootables, args.objcopy)?;
 
     Ok(())
-}
-
-fn parse_args() -> Result<Args> {
-    let mut pico = pico_args::Arguments::from_env();
-
-    if pico.contains(["-h", "--help"]) {
-        // TODO: help
-        // print!("{}", HELP);
-        std::process::exit(0);
-    }
-
-    let args = Args {
-        objcopy: pico.opt_value_from_os_str("--objcopy", self::parse_path)?,
-        systemd_efi_stub: pico.opt_value_from_os_str("--systemd-efi-stub", self::parse_path)?,
-        unified_efi: pico.contains("--unified-efi"),
-        generations: pico
-            .finish()
-            .into_iter()
-            .map(|s| s.into_string().expect("invalid utf8 in generation"))
-            .collect(),
-    };
-
-    match (&args.systemd_efi_stub, &args.objcopy, &args.unified_efi) {
-        (None, None, false) => {}
-        (Some(_), Some(_), true) => {}
-        _ => {
-            return Err(
-                "--systemd-efi-stub, --objcopy, and --unified-efi are required when one or the other is specified"
-                    .into(),
-            );
-        }
-    }
-
-    Ok(args)
-}
-
-fn parse_path(s: &std::ffi::OsStr) -> Result<PathBuf> {
-    Ok(s.into())
 }
