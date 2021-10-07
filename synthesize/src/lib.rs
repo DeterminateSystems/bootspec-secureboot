f1af744d056e28929f6bc84398c1cfc34c4539f7
Cole Helbling <cole.helbling@determinate.systems>
2021-10-07 15:09:00 -0700

    synthesize: add tests

--- synthesize/src/lib.rs
+++ synthesize/src/lib.rs
@@ -85,3 +85,193 @@ pub fn synthesize_schema_from_generation(generation: &Path) -> Result<BootJson> 
         specialisation,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use std::{collections::HashMap, fs};
+
+    use bootspec::{BootJson, SystemConfigurationRoot, JSON_FILENAME, SCHEMA_VERSION};
+    use tempfile::TempDir;
+
+    use super::synthesize_schema_from_generation;
+
+    fn scaffold(
+        system_version: &str,
+        kernel_version: &str,
+        kernel_params: &Vec<String>,
+        specialisations: Option<Vec<&str>>,
+        specialisations_have_boot_spec: bool,
+    ) -> PathBuf {
+        let temp_dir = TempDir::new().expect("Failed to create tempdir for test generation");
+        let generation = temp_dir.into_path();
+
+        fs::create_dir_all(generation.join("kernel-modules/lib/modules"))
+            .expect("Failed to write to test generation");
+        fs::create_dir_all(generation.join("specialisation"))
+            .expect("Failed to write to test generation");
+
+        fs::write(generation.join("nixos-version"), system_version)
+            .expect("Failed to write to test generation");
+        fs::write(generation.join("kernel-modules/bzImage"), "")
+            .expect("Failed to write to test generation");
+        fs::write(
+            generation.join(format!("kernel-modules/lib/modules/{}", kernel_version)),
+            "",
+        )
+        .expect("Failed to write to test generation");
+        fs::write(generation.join("kernel-params"), kernel_params.join(" "))
+            .expect("Failed to write to test generation");
+        fs::write(generation.join("init"), "").expect("Failed to write to test generation");
+        fs::write(generation.join("initrd"), "").expect("Failed to write to test generation");
+        fs::write(generation.join("append-initrd-secrets"), "")
+            .expect("Failed to write to test generation");
+
+        if let Some(specialisations) = specialisations {
+            for spec_name in specialisations {
+                let spec_path = generation.join("specialisation").join(spec_name);
+                fs::create_dir_all(&spec_path).expect("Failed to write to test generation");
+
+                if specialisations_have_boot_spec {
+                    fs::write(spec_path.join(JSON_FILENAME), "")
+                        .expect("Failed to write to test generation");
+                }
+            }
+        }
+
+        generation
+    }
+
+    #[test]
+    fn no_bootspec_no_specialisation() {
+        let system_version = String::from("test-version-1");
+        let kernel_version = String::from("1.1.1-test1");
+        let kernel_params = [
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>();
+
+        let generation = scaffold(
+            &system_version,
+            &kernel_version,
+            &kernel_params,
+            None,
+            false,
+        );
+        let spec = synthesize_schema_from_generation(&generation).unwrap();
+
+        assert_eq!(
+            spec,
+            BootJson {
+                schema_version: SCHEMA_VERSION,
+                system_version,
+                kernel: generation.join("kernel-modules/bzImage"),
+                kernel_version,
+                kernel_params,
+                init: generation.join("init"),
+                initrd: generation.join("initrd"),
+                initrd_secrets: generation.join("append-initrd-secrets"),
+                specialisation: HashMap::new(),
+                toplevel: SystemConfigurationRoot(generation),
+            }
+        );
+    }
+
+    #[test]
+    fn no_bootspec_with_specialisation_no_bootspec() {
+        let system_version = String::from("test-version-2");
+        let kernel_version = String::from("1.1.1-test2");
+        let kernel_params = [
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>();
+        let specialisations = vec!["spec1", "spec2"];
+
+        let generation = scaffold(
+            &system_version,
+            &kernel_version,
+            &kernel_params,
+            Some(specialisations),
+            false,
+        );
+
+        synthesize_schema_from_generation(&generation).unwrap();
+    }
+
+    #[test]
+    fn with_bootspec_no_specialisation() {
+        let system_version = String::from("test-version-3");
+        let kernel_version = String::from("1.1.1-test3");
+        let kernel_params = [
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>();
+
+        let generation = scaffold(
+            &system_version,
+            &kernel_version,
+            &kernel_params,
+            None,
+            false,
+        );
+
+        fs::write(generation.join(JSON_FILENAME), "").expect("Failed to write to test generation");
+
+        let spec = synthesize_schema_from_generation(&generation).unwrap();
+
+        assert_eq!(
+            spec,
+            BootJson {
+                schema_version: SCHEMA_VERSION,
+                system_version,
+                kernel: generation.join("kernel-modules/bzImage"),
+                kernel_version,
+                kernel_params,
+                init: generation.join("init"),
+                initrd: generation.join("initrd"),
+                initrd_secrets: generation.join("append-initrd-secrets"),
+                specialisation: HashMap::new(),
+                toplevel: SystemConfigurationRoot(generation),
+            }
+        );
+    }
+
+    #[test]
+    fn with_bootspec_with_specialisations_with_bootspec() {
+        let system_version = String::from("test-version-4");
+        let kernel_version = String::from("1.1.1-test4");
+        let kernel_params = [
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>();
+        let specialisations = vec!["spec1", "spec2"];
+
+        let generation = scaffold(
+            &system_version,
+            &kernel_version,
+            &kernel_params,
+            Some(specialisations),
+            true,
+        );
+
+        fs::write(generation.join(JSON_FILENAME), "").expect("Failed to write to test generation");
+
+        synthesize_schema_from_generation(&generation).unwrap();
+    }
+}
