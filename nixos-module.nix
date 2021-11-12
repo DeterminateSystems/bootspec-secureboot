a638006443c4adddf3b379220aeb5c24ea2ceb3d
Cole Helbling <cole.helbling@determinate.systems>
2021-11-12 10:48:41 -0800

    README: document how to use with secure boot

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,56 +1,96 @@
 { pkgs, lib, config, ... }:
+let
+  inherit (lib) types;
+in
 {
-  boot.loader.manual = {
-    enable = true;
-    installHook = pkgs.writeShellScript "install-bootloader"
-      (
-        let
-          generatorArgs = lib.escapeShellArgs [
-            "--systemd-machine-id-setup"
-            "${pkgs.systemd}/bin/systemd-machine-id-setup"
-          ];
-
-          installerArgs = lib.escapeShellArgs
-            ([
-              "--esp"
-              config.boot.loader.efi.efiSysMountPoint
-
-              "--console-mode"
-              config.boot.loader.systemd-boot.consoleMode
-
-              "--timeout"
-              (toString config.boot.loader.timeout)
-
-              "--bootctl"
-              "${pkgs.systemd}/bin/bootctl"
-
-              "--generated-entries"
-              "./systemd-boot-entries"
+  options = {
+    boot.loader.secureboot = {
+      enable = lib.mkEnableOption "Secure Boot support";
+      signingKeyPath = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+      };
+      signingCertPath = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+      };
+    };
+  };
+  config = {
+    boot.loader.manual = {
+      enable = true;
+      installHook = pkgs.writeShellScript "install-bootloader"
+        (
+          let
+            generatorArgs = lib.escapeShellArgs [
+              "--systemd-machine-id-setup"
+              "${pkgs.systemd}/bin/systemd-machine-id-setup"
             ]
-            ++ (lib.optional config.boot.loader.efi.canTouchEfiVariables "--touch-efi-vars")
-            ++ (lib.optionals (config.boot.loader.systemd-boot.configurationLimit != null) [
-              "--configuration-limit"
-              "${toString config.boot.loader.systemd-boot.configurationLimit}"
-            ]));
-        in
-        ''
-          set -eux
+            ++ (lib.optionals config.boot.loader.secureboot.enable [
+              "--objcopy"
+              "${pkgs.binutils}/bin/objcopy"
 
-          scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
-          function finish {
-            rm -rf "$scratch"
-          }
-          trap finish EXIT
+              "--systemd-efi-stub"
+              "${pkgs.systemd}/lib/systemd/boot/efi/linuxx64.efi.stub"
+            ]);
 
-          cd "$scratch" || exit 1
+            installerArgs = lib.escapeShellArgs
+              ([
+                "--esp"
+                config.boot.loader.efi.efiSysMountPoint
 
-          ${pkgs.bootspec}/bin/generator /nix/var/nix/profiles/system-*-link \
-            ${generatorArgs}
+                "--console-mode"
+                config.boot.loader.systemd-boot.consoleMode
 
-          ${pkgs.bootspec}/bin/installer \
-            --toplevel="$1" \
-            ${installerArgs}
-        ''
-      );
+                "--timeout"
+                (toString config.boot.loader.timeout)
+
+                "--bootctl"
+                "${pkgs.systemd}/bin/bootctl"
+
+                "--generated-entries"
+                "./systemd-boot-entries"
+              ]
+              ++ (lib.optional config.boot.loader.efi.canTouchEfiVariables "--touch-efi-vars")
+              ++ (lib.optionals (config.boot.loader.systemd-boot.configurationLimit != null) [
+                "--configuration-limit"
+                "${toString config.boot.loader.systemd-boot.configurationLimit}"
+              ])
+              ++ (lib.optionals (config.boot.loader.secureboot.signingKey != null) [
+                "--signing-key"
+                config.boot.loader.secureboot.signingKey
+              ])
+              ++ (lib.optionals (config.boot.loader.secureboot.signingCert != null) [
+                "--signing-key"
+                config.boot.loader.secureboot.signingCert
+              ])
+              ++ (lib.optionals config.boot.loader.secureboot.enable [
+                "--sbsign"
+                "${pkgs.sbsigntool}/bin/sbsign"
+
+                "--sbverify"
+                "${pkgs.sbsigntool}/bin/sbverify"
+              ]));
+          in
+          ''
+            set -eux
+
+            scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+            function finish {
+              rm -rf "$scratch"
+            }
+            trap finish EXIT
+
+            cd "$scratch" || exit 1
+
+            ${pkgs.bootspec}/bin/generator /nix/var/nix/profiles/system-*-link \
+              ${generatorArgs}
+
+            ${pkgs.bootspec}/bin/installer \
+              --toplevel="$1" \
+              ${installerArgs}
+          ''
+        );
+    };
   };
 }
